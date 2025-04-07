e4939114a3beb907e8bdcfc7ad1f1b685c783b28
Saifuddin Sharif <sm.sharif@gmail.com>
2025-04-07 11:19:57 -0600

    All tests working, moved or copied files into different dirs

--- utils/reusable-login.ts
+++ utils/reusable-login.ts
@@ -1,12 +1,12 @@
 import { Page } from "@playwright/test";
 import { LoginPage } from "../pages/loginPage";
 
-export const URL = process.env.UI_BASE_URL || "https://www.saucedemo.com";
-export const USER = process.env.UI_USER || "standard_user";
-export const PASSWORD = process.env.UI_PASSWORD || "secret_sauce";
+export const base_url = process.env.UI_BASE_URL || "https://www.saucedemo.com";
+export const username = process.env.UI_USER || "standard_user";
+export const password = process.env.UI_PASSWORD || "secret_sauce";
 
 export async function reusableLogin(page: Page) {
   const loginPage = new LoginPage(page);
-  await loginPage.goto(URL);
-  await loginPage.login(USER, PASSWORD);
+  await loginPage.goto(base_url);
+  await loginPage.login(username, password);
 }
