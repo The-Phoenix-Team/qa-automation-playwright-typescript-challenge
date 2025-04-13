e977eab84415b9d3693ebbb5bc673c1e2738c600
Saifuddin Sharif <sm.sharif@gmail.com>
2025-04-13 14:09:06 -0600

    After deleting non working specs, all remaining are working

--- tests/cart/cart.spec.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-// test("Some test after login", async ({ loginPage }) => {
-//     // No need to login again, it's handled by the fixture!
-// });
